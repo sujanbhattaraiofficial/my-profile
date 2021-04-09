cf9f0af40c4e064f74ca5d70f084f8e08b022633
sujan bhattarai <sujanb681@gmail.com>
2021-04-09 19:03:21 +0545

    Add live version

--- lib/Bloc/NavBarBloc.dart/navBarBloc.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class NavBarBloc extends Cubit<int> {
-  NavBarBloc(int state) : super(state);
-
-  void changesNavBar(int currentPage) {
-    emit(currentPage);
-  }
-}
